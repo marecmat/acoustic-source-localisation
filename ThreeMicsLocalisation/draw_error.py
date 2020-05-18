03b16f0798f15c1b4556b6f3fc3353ef9d4994ec
marecmat <oupsmajdsl@gmail.com>
2020-05-18 18:35:26 +0200

    heatmaps tests

--- ThreeMicsLocalisation/draw_error.py
+++ ThreeMicsLocalisation/draw_error.py
@@ -1,21 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import TDOALocalisation
+import datetime
 
 # np.random.randint(-100, 100)
 capteurs = [
-        (540, 850),
-        (348, 23),
-        (152, 350),
+        (84, 173),
+        (18, 25),
+        (166, 108),
         ]
-n = 1200 
+n = 200
 mat = np.zeros((n, n))
 
 for xs in range(n):
     print(xs)
     for ys in range(n):
         source_ac = (xs, ys)
-        instance = TDOALocalisation.TDOALocalisation(source_ac, capteurs, T=15, step=1, max_dist=1500)
+        instance = TDOALocalisation.TDOALocalisation(source_ac, capteurs, T=15, step=1, max_dist=600)
         data = instance.get_all_hyperboloid()
         try:
             x_inters, y_inters, mean = instance.intersection_estimation(data)
@@ -26,10 +27,21 @@ for xs in range(n):
         except IndexError:
             print("index error encountered at {}, {}".format(xs, ys)) 
 
-np.savetxt("matrix_otherpos", mat, delimiter=",")
+timestamp = datetime.datetime.now()
+np.savetxt("matrix_otherpos_{}".format(timestamp), mat, delimiter=",")
+mat = np.nan_to_num(mat)
+mat = np.array(mat)
+
+xmax, xmin = mat.max(), mat.min()
+mat = (mat - xmin) / (xmax - xmin)
+print(mat)
+mat[mat == 0] = np.nan
+
 fig, ax = plt.subplots()
+
 im = ax.pcolor(mat)
-fig.colorbar(im)
+cb = fig.colorbar(im)
+cb.set_label('Normalized deviation')
 ax.plot(*zip(*capteurs), 'mo', label='acoustic sensors')
 
 ax.grid()
